2e57c3aac760b1b99ac2d6bcad8f6a9627b5bcbb
Agorgianitis Loukas <agorglouk@gmail.com>
2022-02-09 18:56:07 +0200

    Setup integration between cmp and lspconfig

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -6,6 +6,10 @@ function M.config()
     return
   end
 
+  -- Setup cmp integration
+  local cmp_nvim_lsp = require("cmp_nvim_lsp")
+  local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
   -- Use an on_attach function to only map the following keys
   -- after the language server attaches to the current buffer
   local on_attach = function(client, bufnr)
@@ -26,6 +30,7 @@ function M.config()
   for _, lsp in pairs(servers) do
     plugin[lsp].setup {
       on_attach = on_attach,
+      capabilities = capabilities,
       flags = {
         -- This will be the default in neovim 0.7+
         debounce_text_changes = 150,
