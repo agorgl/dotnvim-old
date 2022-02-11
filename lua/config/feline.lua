3c2fb227012ff99397c286ccc0b51ecfc3a56584
Agorgianitis Loukas <agorglouk@gmail.com>
2022-02-11 17:59:34 +0200

    Modify feline configuration to add separators dynamically

--- lua/config/feline.lua
+++ lua/config/feline.lua
@@ -16,144 +16,138 @@ function M.config()
     }
   end
 
-  local mode_hl = function()
-    return {
-      name = vi_mode_utils.get_mode_highlight_name(),
-      fg = vi_mode_utils.get_mode_color(),
-      bg = 'NONE',
-    }
-  end
-
-  local mode_inverted_hl = function()
-    return {
-      name = vi_mode_utils.get_mode_highlight_name(),
-      bg = vi_mode_utils.get_mode_color(),
-      fg = 'bg',
-      style = 'bold',
-    }
+  local function hl_invert(hl)
+    if type(hl) == 'function' then
+      return function() return hl_invert(hl()) end
+    else
+      return {
+        name = hl.name and hl.name .. '_inverted' or nil,
+        fg = hl.bg,
+        bg = hl.fg,
+      }
+    end
   end
 
   local lighter_hl = hl_from_name("LineNr")
-
-  local lighter_mode_hl = function()
+  local mode_hl = function()
     return {
       fg = vi_mode_utils.get_mode_color(),
       bg = lighter_hl.bg,
     }
   end
 
-  local c = {
-    vimode = {
-      provider = 'vi_mode',
-      hl = mode_inverted_hl,
-      left_sep = {
-        { str = ' ', hl = mode_inverted_hl, }
-      },
-      right_sep = {
-        { str = ' ', hl = mode_inverted_hl, },
-        'right_filled',
-      },
-      icon = '',
-    },
-    gitbranch = {
-      provider = 'git_branch',
-      hl = mode_hl,
-      left_sep = {
-        { str = ' ', hl = mode_hl, }
-      },
-      right_sep = {
-        { str = ' ', hl = mode_hl, },
-        { str = 'right', hl = mode_hl, }
-      },
-      icon = '',
-    },
-    fileinfo = {
-      provider = 'file_info',
-      hl = mode_hl,
-      icon = '',
-    },
-    filetype = {
-      provider = 'file_type',
-      hl = mode_hl,
-      left_sep = {
-        { str = ' ', hl = mode_hl, }
-      },
-      right_sep = {
-        { str = ' ', hl = mode_hl, },
-      },
-      icon = '',
-    },
-    encoding = {
-      provider = 'file_encoding',
-      hl = mode_hl,
-      left_sep = {
-        { str = 'left', hl = mode_hl, },
-        { str = ' ', hl = mode_hl, },
-      },
-      right_sep = {
-        { str = ' ', hl = mode_hl, },
-      },
-      icon = '',
-    },
-    format = {
-      provider = 'file_format',
-      hl = mode_hl,
-      left_sep = {
-        { str = 'left', hl = mode_hl, },
-        { str = ' ', hl = mode_hl, },
-      },
-      right_sep = ' ',
-      icon = '',
-    },
-    lineperc = {
-      provider = 'line_percentage',
-      hl = lighter_hl,
-      left_sep = {
-        'left_filled',
-        { str = ' ', hl = lighter_hl, },
-      },
-      right_sep = {
-        { str = ' ', hl = lighter_hl },
-      },
-    },
-    position = {
-      provider = 'position',
-      hl = lighter_hl,
-      left_sep = {
-        { str = 'left', hl = lighter_hl },
-        { str = ' ', hl = lighter_hl },
-      },
-      right_sep = {
-        { str = ' ', hl = lighter_mode_hl },
-        { str = 'left_filled', hl = lighter_mode_hl },
-      },
-    },
-  }
-
   local active = {
     -- Left
     {
-      c.vimode,
-      c.gitbranch,
-      c.fileinfo,
+      {
+        provider = 'vi_mode',
+        hl = mode_hl,
+        invert = true,
+        icon = '',
+      },
+      {
+        provider = 'git_branch',
+        hl = mode_hl,
+        icon = '',
+      },
+      {
+        provider = 'file_info',
+        hl = mode_hl,
+        icon = '',
+      },
     },
     -- Right
     {
-      c.filetype,
-      c.encoding,
-      c.format,
-      c.lineperc,
-      c.position,
+      {
+        provider = 'file_type',
+        hl = mode_hl,
+        icon = '',
+      },
+      {
+        provider = 'file_encoding',
+        hl = mode_hl,
+        icon = '',
+      },
+      {
+        provider = 'file_format',
+        hl = mode_hl,
+        icon = '',
+      },
+      {
+        provider = 'line_percentage',
+        hl = lighter_hl,
+        icon = '',
+      },
+      {
+        provider = 'position',
+        hl = lighter_hl,
+        icon = '',
+      },
+      {
+        provider = ' ',
+        hl = mode_hl,
+        invert = true,
+        icon = '',
+      }
     }
   }
 
   local inactive = {
     {
-      c.filetype
+      {
+        provider = 'file_type',
+        hl = mode_hl,
+        invert = true,
+        icon = '',
+      },
+      {}
     },
-    -- Empty component to fix the highlight till the end of the statusline
     {}
   }
+
+  -- Surrounds a component with a separator
+  local function surround(c, s)
+    c.left_sep = c.left_sep or {}
+    c.right_sep = c.right_sep or {}
+    table.insert(c.left_sep, 1, { str = s, hl = c.hl })
+    table.insert(c.right_sep, { str = s, hl = c.hl })
+  end
+
+  -- Adds a separator to a component in the given direction
+  local function sepadd(c, s, dir, inverted)
+    c[dir .. '_sep'] = c[dir .. '_sep'] or {}
+    local sep = c[dir .. '_sep']
+    local sep_idx = ({left = 1, right = #sep + 1})[dir]
+    local sep_hl = not inverted and c.hl or nil
+    table.insert(sep, sep_idx, { str = s, hl = sep_hl })
+  end
+
+  -- Styles a component by adding padding and separator
+  local function style(components, direction)
+    local sep = ({
+      left = { normal = 'left', inverted = 'left_filled' },
+      right = { normal = 'right', inverted = 'right_filled' }
+    })[direction]
+
+    for i, component in pairs(components) do
+      if component.provider then
+        local invert = component.invert
+        local mode = invert and 'inverted' or 'normal'
+        local sep = sep[mode]
+
+        component.hl = invert and hl_invert(component.hl) or component.hl
+        surround(component, ' ')
+        sepadd(component, sep, direction, invert)
+      end
+    end
+  end
+
+  for _, group in pairs({active, inactive}) do
+    for j, subgroup in pairs(group) do
+      local direction = ({'right', 'left'})[j]
+      style(subgroup, direction)
+    end
+  end
 
   plugin.setup {
     components = { active = active, inactive = inactive },
