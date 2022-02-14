39c3b9756dbd6c8233237f5090a9f36fef5d23f0
Agorgianitis Loukas <agorglouk@gmail.com>
2022-02-14 16:33:45 +0200

    Modify nvim-tree configuration

--- lua/config/nvimtree.lua
+++ lua/config/nvimtree.lua
@@ -1,12 +1,42 @@
 local M = {}
+local g = vim.g
 
 function M.config()
+  g.nvim_tree_show_icons = {
+    git = 0,
+    folders = 1,
+    files = 1,
+    folder_arrows = 0,
+  }
+
+  local folder_closed, folder_open = '▸', '▾'
+  g.nvim_tree_icons = {
+    default = ' ',
+    symlink = ' ',
+    folder = {
+      arrow_open = '',
+      arrow_closed = '',
+      default = folder_closed,
+      open = folder_open,
+      empty = folder_closed,
+      empty_open = folder_open,
+      symlink = folder_closed,
+      symlink_open = folder_open,
+    }
+  }
+
+  g.nvim_tree_add_trailing = 1
+
   local ok, plugin = pcall(require, "nvim-tree")
   if not ok then
     return
   end
 
-  plugin.setup {}
+  plugin.setup {
+    view = {
+      signcolumn = "no"
+    },
+  }
 end
 
 return M
