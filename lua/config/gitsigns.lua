c8811316c3b594edaf2b50b82e6e2e859a7aa359
Agorgianitis Loukas <agorglouk@gmail.com>
2022-02-16 19:57:06 +0200

    Modify gitsigns configuration

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -6,7 +6,15 @@ function M.config()
     return
   end
 
-  plugin.setup {}
+  plugin.setup {
+    signs = {
+      add          = { hl = 'GitSignsAdd'   , text = '+', numhl = 'GitSignsAddNr'   , linehl = 'GitSignsAddLn' },
+      change       = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+      delete       = { hl = 'GitSignsDelete', text = '-', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+      topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+      changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+    },
+  }
 end
 
 return M
