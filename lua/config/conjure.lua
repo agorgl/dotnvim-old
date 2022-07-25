77d9b7f5c7533fc2796eb5eff9ac6a981e420b1f
Agorgianitis Loukas <agorglouk@gmail.com>
2022-07-25 10:52:29 +0300

    Add autocmd for attaching in cljs shadow process

--- lua/config/conjure.lua
+++ lua/config/conjure.lua
@@ -7,6 +7,14 @@ function M.config()
   g['conjure#client#clojure#nrepl#mapping#refresh_changed'] = false
   g['conjure#client#clojure#nrepl#mapping#refresh_all'] = false
   g['conjure#client#clojure#nrepl#mapping#refresh_clear'] = false
+
+  local api = vim.api
+  local shadow_select_group = api.nvim_create_augroup('cljs_shadow_select', { clear = true })
+  api.nvim_create_autocmd('BufReadPost', {
+    group = shadow_select_group,
+    pattern = '*.cljs',
+    command = 'ConjureShadowSelect app',
+  })
 end
 
 return M
