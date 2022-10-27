153c683ba7abbbd5a4f3aa06f52fce2ea99e5ebf
Agorgianitis Loukas <agorglouk@gmail.com>
2022-10-27 12:07:14 +0300

    Remove deprecated lsp capabilities setup

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -8,7 +8,6 @@ function M.config()
 
   -- Setup cmp integration
   local cmp_nvim_lsp = require("cmp_nvim_lsp")
-  local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
   -- Use an on_attach function to only map the following keys
   -- after the language server attaches to the current buffer
@@ -89,7 +88,6 @@ function M.config()
   for lsp, opts in pairs(servers) do
     plugin[lsp].setup {
       on_attach = on_attach,
-      capabilities = capabilities,
       flags = {
         -- This will be the default in neovim 0.7+
         debounce_text_changes = 150,
