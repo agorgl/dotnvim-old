803a53a6ac74f073820810ba9b3d473f99978e38
Agorgianitis Loukas <agorglouk@gmail.com>
2022-11-29 20:23:58 +0200

    Add sign for untracked lines in gitsigns config

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -13,6 +13,7 @@ function M.config()
       delete       = { hl = 'GitSignsDelete', text = '-', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
       topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
       changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+      untracked    = { hl = 'GitSignsAdd'   , text = '+', numhl = 'GitSignsAddNr'   , linehl = 'GitSignsAddLn' },
     },
   }
 end
