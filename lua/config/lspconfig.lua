5a8f72e9f9f5802cbc2da0afad94a8f368ff2bf2
Agorgianitis Loukas <agorglouk@gmail.com>
2023-02-17 22:34:45 +0200

    Remove filetypes override from tailwindcss lsp config

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -85,7 +85,6 @@ function M.config()
     clojure_lsp = {},
     dartls = {},
     tailwindcss = {
-      filetypes = {"clojure"},
       settings = {
         tailwindCSS = {
           experimental = {
