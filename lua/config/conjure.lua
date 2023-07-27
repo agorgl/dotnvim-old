e23198b4df02c178463d30f59e5d25b279287664
Agorgianitis Loukas <agorglouk@gmail.com>
2023-07-28 01:00:03 +0300

    Rewrite automatic clojure/script session setup

--- lua/config/conjure.lua
+++ lua/config/conjure.lua
@@ -2,44 +2,89 @@ local M = {}
 
 function M.config()
   local g = vim.g
+  local api = vim.api
+  local cmd = vim.cmd
+
+  g['conjure#client_on_load'] = false
   g['conjure#completion#omnifunc'] = false
   g['conjure#completion#fallback'] = false
   g['conjure#client#clojure#nrepl#mapping#refresh_changed'] = false
   g['conjure#client#clojure#nrepl#mapping#refresh_all'] = false
   g['conjure#client#clojure#nrepl#mapping#refresh_clear'] = false
 
-  local api = vim.api
-  local cmd = vim.cmd
-  local wait = function(interval) vim.wait(interval, function() end) end
+  local log = require('conjure.log')
+  local client = require('conjure.client')
+  local server = require('conjure.client.clojure.nrepl.server')
+  local action = require('conjure.client.clojure.nrepl.action')
 
-  local clj_init = false
-  local cljs_attached = false
+  local client_state = function(key)
+    if key == nil or key == '' then
+      return client['state-key']()
+    else
+      return client['set-state-key!'](key)
+    end
+  end
+
+  local connect = function(cb)
+    if not server['connected?']() then
+      return action['connect-port-file']({["silent?"] = true, ["cb"] = cb})
+    else
+      cb()
+    end
+  end
+
+  local is_log_buf = function(path) return log['log-buf?'](path) end
+  local clone_fresh_session = function() action['clone-fresh-session']() end
+  local shadow_select = function(build) action['shadow-select'](build) end
+
+  local clj_initialized = false
+  local cljs_initialized = false
   local conjure_session_setup_group = api.nvim_create_augroup('conjure_session_setup', { clear = true })
   api.nvim_create_autocmd('BufEnter', {
     group = conjure_session_setup_group,
     pattern = {'*.clj', '*.cljc'},
     callback = function()
-      cmd('ConjureClientState clj')
-      clj_init = true
+      local path = vim.fn.expand("%:p")
+      if is_log_buf(path) then
+        return
+      end
+
+      client_state("clj")
+      if not clj_initialized then
+        connect(function()
+          clj_initialized = true
+        end)
+      end
     end
   })
   api.nvim_create_autocmd('BufEnter', {
     group = conjure_session_setup_group,
     pattern = '*.cljs',
     callback = function()
-      if not clj_init then
-        cmd('ConjureClientState clj')
-        wait(150) -- wait for connect
-        clj_init = true
+      local cljs_setup = function()
+        client_state("cljs")
+        if not cljs_initialized then
+          connect(function()
+            vim.schedule(function()
+              clone_fresh_session()
+              vim.defer_fn(function()
+                shadow_select("app")
+                cljs_initialized = true
+              end, 150)
+            end)
+          end)
+        end
       end
-
-      cmd('ConjureClientState cljs')
-      if not cljs_attached then
-        wait(150) -- wait for connect
-        cmd('ConjureCljSessionFresh')
-        wait(150) -- wait for session clone
-        cmd('ConjureShadowSelect app')
-        cljs_attached = true
+      if not clj_initialized then
+        client_state("clj")
+        connect(function()
+          clj_initialized = true
+          vim.defer_fn(function()
+            cljs_setup()
+          end, 150)
+        end)
+      else
+        cljs_setup()
       end
     end
   })
